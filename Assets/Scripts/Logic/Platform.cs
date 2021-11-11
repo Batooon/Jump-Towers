9afb2c58bd78124e463753597982b259917aaeb4
Anton <rozum.ant@gmail.com>
2021-11-12 01:20:41 +0200

    WIP

--- Assets/Scripts/Logic/Platform.cs
+++ Assets/Scripts/Logic/Platform.cs
@@ -1,10 +1,9 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Renderer))]
-public class Platform : MonoBehaviour
+public abstract class Platform : MonoBehaviour
 {
     [field: SerializeField]
     public Transform JumpPoint
@@ -12,9 +11,63 @@ public class Platform : MonoBehaviour
         get;
         private set;
     }
+
+    public abstract bool TryAccept(Player player);
     
     private void OnBecameInvisible()
     {
         gameObject.SetActive(false);
     }
+}
+
+public class StartingPlatform : Platform
+{
+    public override bool TryAccept(Player player)
+    {
+        return true;
+    }
+}
+
+public class FormChangingPlatform : Platform
+{
+    public override bool TryAccept(Player player)
+    {
+        //Если форма игрока совпадает - return true;
+        return true;
+    }
+}
+
+public abstract class PlayerRandomizer
+{
+    protected readonly Player _player;
+    protected readonly PlayerSettings _playerSettings;
+
+    protected PlayerRandomizer(Player player, PlayerSettings playerSettings)
+    {
+        _player = player;
+        _playerSettings = playerSettings;
+    }
+
+    public abstract void Randomize();
+}
+
+public class ColorRandomizer : PlayerRandomizer
+{
+    private int _counter;
+    public ColorRandomizer(Player player, PlayerSettings playerSettings) : base(player, playerSettings)
+    {
+    }
+    
+    public override void Randomize()
+    {
+        var colors = _playerSettings.Colors;
+        _counter %= colors.Count;
+        _player.Renderer.material.color = colors[_counter++];
+    }
+}
+
+[Serializable]
+public class PlayerSettings
+{
+    public List<Color> Colors;
 }
