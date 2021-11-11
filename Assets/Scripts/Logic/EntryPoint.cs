1ed4275b03aa701b200e526103c3f8e8617f9026
Anton <rozum.ant@gmail.com>
2021-11-11 02:39:53 +0200

    Entry Point created

---
+++ Assets/Scripts/Logic/EntryPoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EntryPoint : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    [SerializeField] private PlatformSpawner _platformSpawner;
+    private PlatformWay _platformWay = new PlatformWay();
+
+    private void Awake()
+    {
+        _platformSpawner.Init();
+        for (var i = 0; i < 10; i++)
+        {
+            _platformWay.AddPlatform(_platformSpawner.SpawnPlatform());
+        }
+        
+        _player.Init(_platformWay);
+    }
+}
+
+public class PlatformWay
+{
+    private Queue<Platform> _platforms = new Queue<Platform>();
+
+    public void AddPlatform(Platform newPlatform)
+    {
+        _platforms.Enqueue(newPlatform);
+    }
+
+    public Transform GetNextDestinationPoint()
+    {
+        return _platforms.Dequeue().JumpPoint;
+    }
+}
