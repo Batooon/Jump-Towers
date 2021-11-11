ec5c002c93003be3ae77c27f2abb81c834eee1ba
Anton <rozum.ant@gmail.com>
2021-11-12 01:20:22 +0200

    added color changing platform script

---
+++ Assets/Scripts/Logic/ColorChangingPlatform.cs
@@ -0,0 +1,8 @@
+﻿public class ColorChangingPlatform : Platform
+{
+    public override bool TryAccept(Player player)
+    {
+        //Если цвет игрока совпадает - return true;
+        return true;
+    }
+}
