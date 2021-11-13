9e36ae317ea86bcbed2d2febd6a4420f3fea83a6
Anton <rozum.ant@gmail.com>
2021-11-13 23:02:09 +0200

    Implemented platform factory

---
+++ Assets/Scripts/Logic/GeneralPlatformFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class GeneralPlatformFactory : PlatformFactory
+{
+    [SerializeField] private PlatformConfig _colorChangingPlatform, _formChangingPlatform, _rulesChangingPlatform;
+    public override PlatformConfig GetConfig(PlatformType platformType)
+    {
+        return platformType switch
+        {
+            PlatformType.ColorChanging => _colorChangingPlatform,
+            PlatformType.FormChanging => _formChangingPlatform,
+            PlatformType.RulesChanging => _rulesChangingPlatform,
+            _ => throw new ArgumentOutOfRangeException(nameof(platformType), platformType,
+                $"No config for: {platformType}")
+        };
+    }
+}
---
+++ Assets/Scripts/Logic/PlatformFactory.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public abstract class PlatformFactory : MonoBehaviour
+{
+    public abstract PlatformConfig GetConfig(PlatformType platformType);
+}
